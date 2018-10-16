01fda0ed1b39a5ef8ec8e295f35b5c3f1db87934
Alexandra Corremans <alexandra.corremans@gmail.com>
2018-10-16 12:45:51 +1100

    done!

---
+++ square.rb
@@ -0,0 +1,24 @@
+class Square
+    attr_reader :x, :y, :parent
+
+    def initialize(x,y,parent=nil)
+        @x = x
+        @y = y
+        @parent = parent
+        @children = []
+    end
+
+    def display
+        puts "[#{@x},#{@y}]"
+    end
+
+    def generate_children
+        # returns an array of square objects containing all next moves coming from the current square
+        moves = [[-2,-1],[-2,1],[-1,-2],[-1,2],[1,-2],[1,2],[2,-1],[2,1]]
+        moves.each do |move|
+            @children.push(Square.new(x+move[0],y+move[1],self)) if ((x+move[0]).between?(0,7) and (y+move[1]).between?(0,7))
+        end
+    return @children
+    end
+    
+end
